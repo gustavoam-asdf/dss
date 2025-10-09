9ada1705a6fd6b43c92ae481e61032d12285570e
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2025-10-09 13:33:45 +0200

    DSS-3710 : add sigRTst and refTst message-imprint computation + add alternative message imprint computation (proposed in the comments to the CB-AdES draft)

--- dss-cb-ades/src/main/java/eu/europa/esig/dss/cbades/validation/timestamp/CBAdESTimestampMessageDigestBuilder.java
+++ dss-cb-ades/src/main/java/eu/europa/esig/dss/cbades/validation/timestamp/CBAdESTimestampMessageDigestBuilder.java
@@ -2,6 +2,7 @@ package eu.europa.esig.dss.cbades.validation.timestamp;
 
 import co.nstant.in.cbor.model.UnicodeString;
 import eu.europa.esig.dss.cbades.CBAdESUtils;
+import eu.europa.esig.dss.cbades.COSEConstants;
 import eu.europa.esig.dss.cbades.COSEProtectedHeader;
 import eu.europa.esig.dss.cbades.COSESignatureContext;
 import eu.europa.esig.dss.cbades.cbor.CBORArray;
@@ -28,8 +29,11 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Builds the message-imprint digest for CB-AdES timestamps
@@ -106,9 +110,61 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
     @Override
     public DSSMessageDigest getContentTimestampMessageDigest() {
         try {
+            /*
+             * The message imprint computation input for the time-stamp token shall be
+             * an octet stream built as indicated below:
+             * 1) If the sigD header parameter, as specified in clause 5.2.8 of the present document,
+             * is absent then the message imprint computation input shall be the COSE Payload value.
+             * 2) Else, if the sigD header parameter is present, if the value of its mId member is
+             * "http://uri.etsi.org/19152/ObjectIdByURI" or "http://uri.etsi.org/19152/ObjectIdByURIHash"
+             * then concatenate the bytes resulting from processing the contents of its pars member
+             * as specified in clause 5.2.8.2.2 of the present document.
+             * 3) Else, if the value of its mId member is neither "http://uri.etsi.org/19152/ObjectIdByURI"
+             * nor "http://uri.etsi.org/19152/ObjectIdByURIHash", then it is out of the scope of the present
+             * document to specify how to retrieve the COSE Payload, and the specification defining
+             * the value of the mId member shall have to specify how to retrieve the COSE Payload.
+             */
             DSSMessageDigestCalculator digestCalculator = new DSSMessageDigestCalculator(digestAlgorithm);
             writeSignedDataBinaries(digestCalculator);
-            return digestCalculator.getMessageDigest(digestAlgorithm);
+            DSSMessageDigest messageDigest = digestCalculator.getMessageDigest(digestAlgorithm);
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("The 'adoTst' timestamp message-imprint : {}", messageDigest);
+            }
+            return messageDigest;
+
+            // TODO : alternative proposed
+//            CBORByteString payload;
+//            /*
+//             * The message imprint computation input for the time-stamp token shall be
+//             * an octet stream built as indicated below:
+//             *
+//             * 1) If the sigD, as specified in clause 5.2.8 of the present document,
+//             * header parameter is absent, then the message imprint computation input shall be:
+//             * - The CBOR byte string of the payload field, if the payload field is present.
+//             * - Else, the bytes of the detached COSE Payload, encapsulated in a CBOR byte string
+//             *   (COSE Payload is detached, and not explicitly referenced by the sigD header parameter).
+//             */
+//
+//            SigDMechanism sigDMechanism = signature.getSigDMechanism();
+//            if (sigDMechanism == null) {
+//                payload = getPayload();
+//            }
+//            /*
+//             * 2) If the sigD header parameter is present, the message imprint computation input
+//             * shall be the bytes resulting from processing the contents of its pars member as
+//             * specified in clause 5.2.8.2.2 of the present document, concatenated and encapsulated
+//             * in a CBOR byte string.
+//             */
+//            else {
+//                payload = getSigDReferencedOctets(sigDMechanism);
+//            }
+//
+//            byte[] serializedCborObject = CBORUtils.serializeCborObject(payload);
+//            if (LOG.isTraceEnabled()) {
+//                LOG.trace("The 'adoTst' timestamp message-imprint : {}", Utils.toBase64(serializedCborObject));
+//            }
+//            byte[] digestValue = DSSUtils.digest(digestAlgorithm, serializedCborObject);
+//            return new DSSMessageDigest(digestAlgorithm, digestValue);
 
         } catch (Exception e) {
             String errorMessage = timestampToken == null ? String.format(MESSAGE_IMPRINT_ERROR, e.getMessage()) :
@@ -174,6 +230,9 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
     @Override
     public DSSMessageDigest getSignatureTimestampMessageDigest() {
         try {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("--->Get 'sigTst' timestamp data : {}", timestampToken == null ? "--> CREATION" : "--> VALIDATION");
+            }
             /*
              * The input of the message imprint computation for the time-stamp tokens
              * encapsulated by sigTst CBOR map shall be the COSE signature value present
@@ -182,9 +241,20 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
              *       CBOR byte string member of instances of COSE_Signature type specified
              *       in IETF RFC 9052 [2] clause 4.1.
              */
-            // TODO : review the message-imprint computation -> should be CBOR encoded?
             byte[] signatureTimestampData = getSignatureValue();
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("The 'sigTst' timestamp message-imprint : {}", Utils.toBase64(signatureTimestampData));
+            }
             return new DSSMessageDigest(digestAlgorithm, DSSUtils.digest(digestAlgorithm, signatureTimestampData));
+
+            // TODO : review the message-imprint computation -> should be CBOR encoded?
+//            CBORByteString signatureValue = signature.getCoseSignature().getSignature();
+//            byte[] serializedCborObject = CBORUtils.serializeCborObject(signatureValue);
+//            if (LOG.isTraceEnabled()) {
+//                LOG.trace("The 'sigTst' timestamp message-imprint : {}", Utils.toBase64(serializedCborObject));
+//            }
+//            byte[] digestValue = DSSUtils.digest(digestAlgorithm, serializedCborObject);
+//            return new DSSMessageDigest(digestAlgorithm, digestValue);
 
         } catch (Exception e) {
             String errorMessage = timestampToken == null ? String.format(MESSAGE_IMPRINT_ERROR, e.getMessage()) :
@@ -204,12 +274,198 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
 
     @Override
     public DSSMessageDigest getTimestampX1MessageDigest() {
-        return null;
+        try {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("--->Get 'sigRTst' timestamp data : {}", timestampToken == null ? "--> CREATION" : "--> VALIDATION");
+            }
+
+            final DSSMessageDigestCalculator digestCalculator = new DSSMessageDigestCalculator(digestAlgorithm);
+
+            /*
+             * The message imprint computation input shall be the concatenation of the components,
+             * in the order they are listed below:
+             *
+             * 1) The COSE signature value.
+             */
+            digestCalculator.update(getSignatureValue());
+
+            /*
+             * 2) Those CBOR-bstr-wrapped values among the following elements that appear before sigRTst,
+             * in their order of appearance within the uHeaders parameter:
+             * - sigTst if it is present;
+             * - refs if it is present
+             */
+            CBAdESUHeaders uHeaders = signature.getUHeaders();
+            List<CBAdESUHeadersComponent> uHeadersToBeCovered = getUHeadersToBeCovered(uHeaders,
+                    COSEConstants.SIG_TST, COSEConstants.REFS);
+            if (Utils.isCollectionNotEmpty(uHeadersToBeCovered)) {
+                for (CBAdESUHeadersComponent uHeaderComponent : uHeadersToBeCovered) {
+                    digestCalculator.update(getUHeadersComponentValue(uHeaderComponent));
+                }
+            }
+
+            final DSSMessageDigest messageDigest = digestCalculator.getMessageDigest(digestAlgorithm);
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("The 'sigRTst' timestamp message-imprint : {}", messageDigest);
+            }
+            return messageDigest;
+
+            // TODO : temp proposal (similar to 'arcTst' message-imprint computation)
+//            CBORSignature cose = signature.getCoseSignature();
+//
+//            /*
+//             * 1) Initialize an empty CBOR array.
+//             */
+//            final CBORArray array = new CBORArray();
+//
+//            /*
+//             * 2) Add the CBOR byte string in the signature component.
+//             */
+//            array.add(cose.getSignature());
+//
+//            /*
+//             * 3) If the CB-AdES signature is built on the COSE_Sign structure, take those elements
+//             * in the uHeaders header parameter from the signer layer in the order that they appear
+//             * within uHeaders, and add them to the CBOR array:
+//             * - sigTst if it is present;
+//             * - refs if it is present.
+//             *
+//             * If the signer layer does not have any of those uHeaders header parameters,
+//             * add a zero-length CBOR byte string.
+//             *
+//             * 4) If the CB-AdES signature is built on the COSE_Sign1 structure, take those elements
+//             * in the uHeaders header parameter from the body layer in the order that they appear within uHeaders,
+//             * and add them to the CBOR array:
+//             * - sigTst if it is present;
+//             * - refs if it is present.
+//             *
+//             * If the signer layer does not have any of those uHeaders header parameters,
+//             * add a zero-length CBOR byte string.
+//             */
+//            uHeadersToBeCovered = getUHeadersToBeCovered(uHeaders,
+//                    COSEConstants.SIG_TST, COSEConstants.REFS);
+//            if (Utils.isCollectionNotEmpty(uHeadersToBeCovered)) {
+//                for (CBAdESUHeadersComponent uHeaderComponent : uHeadersToBeCovered) {
+//                    array.add(uHeaderComponent.getComponent());
+//                }
+//
+//            } else {
+//                array.add(CBORUtils.EMPTY_BYTE_STRING);
+//            }
+//
+//            /*
+//             * 5) Encode the generated CBOR array in a CBOR byte string.
+//             */
+//
+//            byte[] serializedCborObject = CBORUtils.serializeCborObject(array);
+//            if (LOG.isTraceEnabled()) {
+//                LOG.trace("The 'sigRTst' timestamp message-imprint : {}", Utils.toBase64(serializedCborObject));
+//            }
+//            byte[] digestValue = DSSUtils.digest(digestAlgorithm, serializedCborObject);
+//            return new DSSMessageDigest(digestAlgorithm, digestValue);
+
+        } catch (Exception e) {
+            String errorMessage = timestampToken == null ? String.format(MESSAGE_IMPRINT_ERROR, e.getMessage()) :
+                    String.format(MESSAGE_IMPRINT_ERROR_WITH_ID, timestampToken.getDSSIdAsString(), e.getMessage());
+            if (LOG.isDebugEnabled()) {
+                LOG.warn(errorMessage, e);
+            } else {
+                LOG.warn(errorMessage);
+            }
+        }
+        return DSSMessageDigest.createEmptyDigest();
     }
 
     @Override
     public DSSMessageDigest getTimestampX2MessageDigest() {
-        return null;
+        try {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("--->Get 'rfsTst' timestamp data : {}", timestampToken == null ? "--> CREATION" : "--> VALIDATION");
+            }
+
+            final DSSMessageDigestCalculator digestCalculator = new DSSMessageDigestCalculator(digestAlgorithm);
+
+            /*
+             * The message imprint computation input shall be the concatenation of those CBOR-bstr-wrapped
+             * values among the following elements that appear before sigRTst, in their order of appearance
+             * within the uHeaders header parameter:
+             * - sigTst if it is present;
+             * - refs if it is present
+             */
+            CBAdESUHeaders uHeaders = signature.getUHeaders();
+            List<CBAdESUHeadersComponent> uHeadersToBeCovered = getUHeadersToBeCovered(uHeaders,
+                    COSEConstants.SIG_TST, COSEConstants.REFS);
+            if (Utils.isCollectionNotEmpty(uHeadersToBeCovered)) {
+                for (CBAdESUHeadersComponent uHeaderComponent : uHeadersToBeCovered) {
+                    digestCalculator.update(getUHeadersComponentValue(uHeaderComponent));
+                }
+            }
+
+            final DSSMessageDigest messageDigest = digestCalculator.getMessageDigest(digestAlgorithm);
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("The 'rfsTst' timestamp message-imprint : {}", messageDigest);
+            }
+            return messageDigest;
+
+            // TODO : temp proposal (similar to 'arcTst' message-imprint computation)
+//            CBORSignature cose = signature.getCoseSignature();
+//
+//            /*
+//             * 1) Initialize an empty CBOR array.
+//             */
+//            final CBORArray array = new CBORArray();
+//
+//            /*
+//             * 2) If the CB-AdES signature is built on the COSE_Sign structure, take those elements
+//             * in the uHeaders header parameter from the signer layer in the order that they appear
+//             * within uHeaders, and add them to the CBOR array:
+//             * - sigTst if it is present;
+//             * - refs if it is present.
+//             *
+//             * If the signer layer does not have any of those uHeaders header parameters,
+//             * add a zero-length CBOR byte string.
+//             *
+//             * 3) If the CB-AdES signature is built on the COSE_Sign1 structure, take those elements
+//             * in the uHeaders header parameter from the body layer in the order that they appear within uHeaders,
+//             * and add them to the CBOR array:
+//             * - sigTst if it is present;
+//             * - refs if it is present.
+//             *
+//             * If the signer layer does not have any of those uHeaders header parameters,
+//             * add a zero-length CBOR byte string.
+//             */
+//            uHeadersToBeCovered = getUHeadersToBeCovered(uHeaders,
+//                    COSEConstants.SIG_TST, COSEConstants.REFS);
+//            if (Utils.isCollectionNotEmpty(uHeadersToBeCovered)) {
+//                for (CBAdESUHeadersComponent uHeaderComponent : uHeadersToBeCovered) {
+//                    array.add(uHeaderComponent.getComponent());
+//                }
+//
+//            } else {
+//                array.add(CBORUtils.EMPTY_BYTE_STRING);
+//            }
+//
+//            /*
+//             * 4) Encode the generated CBOR array in a CBOR byte string.
+//             */
+//
+//            byte[] serializedCborObject = CBORUtils.serializeCborObject(array);
+//            if (LOG.isTraceEnabled()) {
+//                LOG.trace("The 'refTst' timestamp message-imprint : {}", Utils.toBase64(serializedCborObject));
+//            }
+//            byte[] digestValue = DSSUtils.digest(digestAlgorithm, serializedCborObject);
+//            return new DSSMessageDigest(digestAlgorithm, digestValue);
+
+        } catch (Exception e) {
+            String errorMessage = timestampToken == null ? String.format(MESSAGE_IMPRINT_ERROR, e.getMessage()) :
+                    String.format(MESSAGE_IMPRINT_ERROR_WITH_ID, timestampToken.getDSSIdAsString(), e.getMessage());
+            if (LOG.isDebugEnabled()) {
+                LOG.warn(errorMessage, e);
+            } else {
+                LOG.warn(errorMessage);
+            }
+        }
+        return DSSMessageDigest.createEmptyDigest();
     }
 
     @Override
@@ -227,7 +483,7 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
              * For computing the input to the message imprint computation, indicated in step 2) in clause 5.3.5.2,
              * the steps listed below shall be performed:
              *
-             *  1) Initialize an empty CBOR array.
+             * 1) Initialize an empty CBOR array.
              */
             final CBORArray array = new CBORArray();
 
@@ -323,12 +579,17 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
              * header parameter, add a zero-length CBOR byte string.
              */
             CBAdESUHeaders uHeaders = signature.getUHeaders();
-            for (CBAdESUHeadersComponent uHeaderComponent : uHeaders.getAttributes()) {
-                if (timestampAttribute != null && timestampAttribute.equals(uHeaderComponent)) {
-                    // the timestamp is reached, stop the iteration
-                    break;
+            if (uHeaders.isExist()) {
+                for (CBAdESUHeadersComponent uHeaderComponent : uHeaders.getAttributes()) {
+                    if (timestampAttribute != null && timestampAttribute.equals(uHeaderComponent)) {
+                        // the timestamp is reached, stop the iteration
+                        break;
+                    }
+                    array.add(uHeaderComponent.getComponent());
                 }
-                array.add(uHeaderComponent.getComponent());
+
+            } else {
+                array.add(CBORUtils.EMPTY_BYTE_STRING);
             }
 
             byte[] serializedCborObject = CBORUtils.serializeCborObject(array);
@@ -348,6 +609,22 @@ public class CBAdESTimestampMessageDigestBuilder implements TimestampMessageDige
             }
         }
         return DSSMessageDigest.createEmptyDigest();
+    }
+
+    private List<CBAdESUHeadersComponent> getUHeadersToBeCovered(CBAdESUHeaders uHeaders, Long... allowedTypes) {
+        if (uHeaders != null && uHeaders.isExist()) {
+            return uHeaders.getAttributes().stream().filter(h -> isAllowedTypeEntry(h, allowedTypes)).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    private boolean isAllowedTypeEntry(CBAdESUHeadersComponent uHeaderComponent, Long... allowedTypes) {
+        return Arrays.asList(allowedTypes).contains(uHeaderComponent.getHeaderId());
+    }
+
+    private byte[] getUHeadersComponentValue(CBAdESUHeadersComponent uHeaderComponent) {
+        CBORObject component = uHeaderComponent.getComponent();
+        return ((CBORByteString) component).getBytes();
     }
     
 }
