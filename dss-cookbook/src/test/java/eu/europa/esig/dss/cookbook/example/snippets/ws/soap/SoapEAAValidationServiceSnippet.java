d307456a85b6e1cfd9c8a1fbb8fadda88163a1ce
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2026-06-19 16:46:23 +0200

    DSS-3905 : update rest and soap samples, add documentation

---
+++ dss-cookbook/src/test/java/eu/europa/esig/dss/cookbook/example/snippets/ws/soap/SoapEAAValidationServiceSnippet.java
@@ -0,0 +1,53 @@
+package eu.europa.esig.dss.cookbook.example.snippets.ws.soap;
+
+// tag::demo[]
+import eu.europa.esig.dss.cookbook.example.CookbookTools;
+import eu.europa.esig.dss.model.FileDocument;
+import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
+import eu.europa.esig.dss.token.SignatureTokenConnection;
+import eu.europa.esig.dss.ws.converter.RemoteDocumentConverter;
+import eu.europa.esig.dss.ws.dto.RemoteDocument;
+import eu.europa.esig.dss.ws.eaa.validation.dto.EAAToValidateDTO;
+import eu.europa.esig.dss.ws.eaa.validation.dto.EAAValidationParametersDTO;
+import eu.europa.esig.dss.ws.eaa.validation.soap.SoapEAAValidationServiceImpl;
+import eu.europa.esig.dss.ws.eaa.validation.soap.client.SoapEAAValidationService;
+import eu.europa.esig.dss.ws.validation.dto.WSReportsDTO;
+
+public class SoapEAAValidationServiceSnippet extends CookbookTools {
+
+    @SuppressWarnings("unused")
+    public void demo() throws Exception {
+
+        try (SignatureTokenConnection signingToken = getPkcs12Token()) {
+
+            DSSPrivateKeyEntry privateKey = signingToken.getKeys().get(0);
+
+            // Instantiate the REST client
+            SoapEAAValidationService soapClient = new SoapEAAValidationServiceImpl();
+
+            // Initialize EAA document to be validated
+            FileDocument signatureToValidate = new FileDocument("src/test/resources/mdoc-eaa.cbor");
+            RemoteDocument signedDocument = RemoteDocumentConverter.toRemoteDocument(signatureToValidate);
+
+            // Initialize validation parameters, when needed
+            EAAValidationParametersDTO validationParameters = new EAAValidationParametersDTO();
+
+            // E.g. provide SessionTranscript, required for mdoc's key binding signature validation
+            FileDocument sessionTranscript = new FileDocument("src/test/resources/sessionTranscript.cbor");
+            validationParameters.setSessionTranscript(RemoteDocumentConverter.toRemoteDocument(sessionTranscript));
+
+            // Initialize XML validation policy to be used (optional, if not provided the default policy will be used)
+            FileDocument policyFile = new FileDocument("src/test/resources/policy.xml");
+            RemoteDocument policy = RemoteDocumentConverter.toRemoteDocument(policyFile);
+            
+            // Create the object containing data to be validated
+            EAAToValidateDTO toValidate = new EAAToValidateDTO(signedDocument, validationParameters, policy);
+
+            // Validate the EAA Presentation
+            WSReportsDTO result = soapClient.validateEAA(toValidate);
+        }
+
+    }
+
+}
+// end::demo[]
